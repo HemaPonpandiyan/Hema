e2a91cbb6c297d67bfc5300e36514b4b91f4293b
HemaPonpandiyan <98088368+HemaPonpandiyan@users.noreply.github.com>
2022-01-21 18:39:56 +0530

    Add files via upload

---
+++ EmployeeDetails/src/org/emp/Employee.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public class Employee {
+	public static void main(String[] args) {
+		Employee ee=new Employee();
+		ee.empId();
+		ee.empName();
+		ee.empDob();
+		ee.empEmail();
+		ee.empAddress();		
+	}
+	private void empId() {
+		// TODO Auto-generated method stub
+     System.out.println("Employee ID: 6");
+	}
+	private void empName() {
+		// TODO Auto-generated method stub
+     System.out.println("Employee Name: Hema");
+	}
+	private void empDob() {
+		// TODO Auto-generated method stub
+     System.out.println("Employee DOB: 06/02/1993");
+	}
+	private void empEmail() {
+		// TODO Auto-generated method stub
+     System.out.println("Employee Email ID: Hema.ponpandiyan@gmail.com");
+	}
+	private void empAddress() {
+		// TODO Auto-generated method stub
+     System.out.println("Employee Address: No 12, big street, Kundrathur");
+	}
+}
