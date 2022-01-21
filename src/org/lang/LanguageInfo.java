80a08c9c12a59583b97ccbae9298c20585bcbabf
HemaPonpandiyan <98088368+HemaPonpandiyan@users.noreply.github.com>
2022-01-21 19:10:02 +0530

    Add files via upload

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,18 @@
+package org.lang;
+
+public class LanguageInfo {
+public static void main(String[] args) {
+	}
+public void TamilLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("Tamil");
+}
+public void EnglishLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("English");
+}
+public void HindiLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("Hindi");
+}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class StateDetails {
+public static void main(String[] args) {
+	LanguageInfo lninfo= new LanguageInfo();
+	lninfo.TamilLanguage();
+	lninfo.EnglishLanguage();
+	lninfo.HindiLanguage();
+	StateDetails st=new StateDetails();
+	st.southIndia();
+	st.NothIndia();
+}
+private void southIndia() {
+	// TODO Auto-generated method stub
+System.out.println("TamilNadu");
+}
+private void NothIndia() {
+	// TODO Auto-generated method stub
+System.out.println("J&K");
+}
+}
